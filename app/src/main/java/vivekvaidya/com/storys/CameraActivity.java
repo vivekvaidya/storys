88ac07f1857cfda4bcd94446ec33e475b3962f52
vivekvaidya <vivektheilliterate@gmail.com>
2017-04-29 17:24:56 -0500

    saving progress

--- app/src/main/java/vivekvaidya/com/storys/CameraActivity.java
+++ app/src/main/java/vivekvaidya/com/storys/CameraActivity.java
@@ -19,6 +19,7 @@ public class CameraActivity extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
